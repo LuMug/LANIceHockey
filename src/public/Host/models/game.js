8eab1beff2d8d149987f77354864d70649910c97
NathFerrari <nathan.ferrari@samtrevano.ch>
2022-05-02 09:18:20 +0200

    porte

--- src/public/Host/models/game.js
+++ src/public/Host/models/game.js
@@ -53,13 +53,13 @@ export default class Game extends Phaser.Scene {
 
         var borders = Array(highSide, lowSide, leftSide, rightSide /*, angleA, angleB, angleC, angleD*/ );
         //lati porte solidi
-        borders.push(new Phaser.GameObjects.Rectangle(this, raggioAngoli, SET_HEIGHT / 2 - raggioAngoli, 2, raggioAngoli/1.5));
+        borders.push(new Phaser.GameObjects.Rectangle(this, raggioAngoli/2, SET_HEIGHT / 2 - raggioAngoli/2, 2, raggioAngoli/1.5));
         borders.push(new Phaser.GameObjects.Rectangle(this, raggioAngoli, SET_HEIGHT / 2 - raggioAngoli / 2, raggioAngoli/2, 2));
         borders.push(new Phaser.GameObjects.Rectangle(this, raggioAngoli, SET_HEIGHT / 2 + raggioAngoli / 2, raggioAngoli/2, 2));
 
-        borders.push(new Phaser.GameObjects.Rectangle(this, SET_WIDTH - raggioAngoli, SET_HEIGHT / 2 - raggioAngoli / 2, 2, raggioAngoli/1.5));
+        borders.push(new Phaser.GameObjects.Rectangle(this, SET_WIDTH - raggioAngoli/2, SET_HEIGHT / 2 - raggioAngoli, 2, raggioAngoli/1.5));
         borders.push(new Phaser.GameObjects.Rectangle(this, SET_WIDTH - raggioAngoli / 2 * 3, SET_HEIGHT / 2 - raggioAngoli / 2, raggioAngoli/2, 2));
-        borders.push(new Phaser.GameObjects.Rectangle(this, SET_WIDTH - raggioAngoli / 2 * 3 + raggioAngoli, SET_HEIGHT / 2 - raggioAngoli / 2, raggioAngoli/2, 2));
+        borders.push(new Phaser.GameObjects.Rectangle(this, SET_WIDTH - raggioAngoli / 2 * 3 , SET_HEIGHT / 2 - raggioAngoli / 2 * 3, raggioAngoli/2, 2));
 
         this.bordersGroup = this.physics.add.staticGroup();
         for (var i = 0; i < borders.length; i++) {
