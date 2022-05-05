adbd31f62c2e29a0b3c4e8a54fdcd93749390060
xavier.horisberger <xavier.horisberger@CPT.local>
2022-05-05 10:32:18 +0200

    diagrammaClassi

--- src/public/Host/script/hostConnection.js
+++ src/public/Host/script/hostConnection.js
@@ -3,6 +3,7 @@
 export default class Host {
     //ws = new WebSocket("ws://10.90.1.117:2389");
     ws = new WebSocket("ws://127.0.0.1:2389");
+
     constructor(game) {
         this.ws.addEventListener("open", () => {
             console.log("We are connected!");
